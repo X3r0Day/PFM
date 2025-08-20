a603d9c671df41a3dcc322f8f197ab704a05aca6
X3r0Day <arnav4727@outlook.com>
2025-08-20 23:49:28 +0530

    Fixed printing logic

--- decorator.py
+++ decorator.py
@@ -2,13 +2,13 @@ from colorama import Style, Fore
 
 class log:
     @staticmethod
-    def suc(msg): return f"{Style.BRIGHT}{Fore.GREEN}[+] {Style.RESET_ALL}{msg}"
+    def suc(msg):  print(f"{Style.BRIGHT}{Fore.GREEN}[+] {Style.RESET_ALL}{msg}")
 
     @staticmethod
-    def warn(msg): return f"{Fore.YELLOW}[-] {Style.RESET_ALL}{msg}"
+    def warn(msg): print(f"{Fore.YELLOW}[-] {Style.RESET_ALL}{msg}")
 
     @staticmethod
-    def err(msg): return f"{Style.BRIGHT}{Fore.RED}[x] {Style.RESET_ALL}{msg}"
+    def err(msg):  print(f"{Style.BRIGHT}{Fore.RED}[x] {Style.RESET_ALL}{msg}")
 
     @staticmethod
-    def info(msg): return f"{Fore.BLUE}[!] {Style.RESET_ALL}{msg}"
+    def info(msg): print(f"{Fore.BLUE}[!] {Style.RESET_ALL}{msg}")
